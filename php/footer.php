734e0b00ab6fcc8991a69da22f78d1398fff8525
Mac Doussias <macdd@me.com>
2018-08-05 20:09:02 -0700

    footer
    Initial Commit
    This footer can be include

---
+++ php/footer.php
@@ -0,0 +1,23 @@
+<?php
+    echo "      
+                <style>
+                    .footerId{
+                    color: #043308;
+                    text-align: center;
+                    font-size: .7em;
+                    }
+                </style>
+                
+                
+                <footer class='footerId'>
+                    <hr>
+		            <p>
+		            CSUMB CST 336. 2018&copy; Team Makesmart Team Final Project <br />
+		            <strong>Disclaimer:</strong><br /> 
+		            The information in this webpage is fictitious. <br />
+			        This site is for academic purposes only. <br />
+		            All plant description and images are from research retrieved August 5th, 2015 from: <br />
+                    <a class='footerId' href='https://en.wikipedia.org/wiki/List_of_poisonous_plants'>https://en.wikipedia.org/wiki/List_of_poisonous_plants</a>
+               <footer>
+        ";
+?>                
