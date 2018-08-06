e0f14535d2eb1035084d33e2ac81e45ebb576859
pavlos <p.pp256@yahoo.com>
2018-08-06 06:43:22 +0000

    added error check,named parameters, modal for report

--- admin/admin.php
+++ admin/admin.php
@@ -11,88 +11,90 @@ if (!isset($_SESSION['adminName'])) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="styles/main.css" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Nosifer" rel="stylesheet">
         <title>Admin Main Page</title>
     </head>
 
-
     <body > 
-    
-         <nav class="navbar navbar-expand-lg navbar-dark">
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <a class="navbar-brand" >
                 <span id="teeny-shop">Teeny Shop</span>
                 <span id="of"> of</span>
                 <span id="horrors"> Horrors</span>
-                
-                <!--<span><a href="../CST336FINAL/admin/login.php"><input class="btn navbar-btn" type="button" value="Store"></input></a></span>-->
-            
             </a>
         </nav>
-        
+
         <div class='container'>
-          <div class="dropdown">
-            <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">REPORTS
-                <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                <li><a id="avg">AVG  PRICE</a></li>
-                <li><a id ="max">MAX  PRICE</a></li>
-                <li><a id ="sum">SUM PRICES</a></li>
-                <li><a style="color:blue;" id ="clear">clear</a></li>
-            </ul>
-        </div>
-        
-
-        <div  class="text-center">
-            <h1> Admin Main Page</h1>
-            <!--<h4>Welcome Administrator <i><?= $_SESSION['adminName'] ?></i></h4><hr>-->
-        </div>
-        
-       
-        <div id="first" class='container'>
-            
-        <div class='text-center' id="options">
-            <div id="displaysum"></div>
-            <div id="displayavg"></div>
-            <div id="displaymax"></div>
-            <br>
-        </div> 
-    
-            <?php
-
-            function displayAllProducts() {
-                include "dbConnection.php";
-                $conn = getDatabaseConnection();
-
-                $sql = "SELECT * FROM plant ORDER BY idplant";
-                $stmt = $conn->prepare($sql);
-                $stmt->execute();
-                $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-                return $records;
-            }
-
-            $records = displayAllProducts();
+            <div class="dropdown">
+                <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown" >REPORTS
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li data-toggle="modal" data-target="#myModal"><a href="#" id="avg">AVG  PRICE</a></li>
+                    <li data-toggle="modal" data-target="#myModal"><a href="#" id ="max">MAX  PRICE</a></li>
+                    <li data-toggle="modal" data-target="#myModal"><a href="#" id ="sum">SUM PRICES</a></li>
+                </ul>
+            </div>
 
 
-            echo "<div class='container' >";
+            <div  class="text-center">
+                <h1> Admin Main Page</h1>
+            </div>
 
-            echo "<div class='span2' >
+
+            <div>
+
+                <div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content" style="background-color:gray;">
+                            <div class="modal-body"> 
+                                <div class='text-center' id="dialog-modal">
+                                    <div id="displaysum"></div>
+                                    <div id="displayavg"></div>
+                                    <div id="displaymax"></div>
+                                    <br>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <?php
+
+                function displayAllProducts() {
+                    include "dbConnection.php";
+                    $conn = getDatabaseConnection();
+
+                    $sql = "SELECT * FROM plant ORDER BY idplant";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    return $records;
+                }
+
+                $records = displayAllProducts();
+
+
+                echo "<div class='container' >";
+
+                echo "<div class='span2' >
                     <form action='addProduct.php'>
                         <button type='submit' class='btn btn-success' id='beginning' name='addProduct' value='Add Product'>Add Product</button>
                         <input type='submit' style='float:right;' class='btn btn-warning' id='beginning' name='logOut' value='  Log Out  '/>
                     </form>
                  </div>";
 
-            echo "<table class='table table-hover'>";
+                echo "<table class='table table-hover'>";
 
-            echo
-            "<thead>
+                echo
+                "<thead>
                 <tr>
                    <th scope = 'col'>Image</th>
                    <th scope = 'col'>Plant Name</th>
@@ -104,51 +106,52 @@ if (!isset($_SESSION['adminName'])) {
               </thead>";
 
 
-            echo "<tbody>";
-            
-            foreach ($records as $record) {
-            echo"<div id='$counter'>";
-                echo "<tr>";
-                
-                $img = "../".$record['imgLink'];
+                echo "<tbody>";
 
-                echo "<td><a target='_blank' href=".$img."><img src=" . $img . " alt='No Image' class='imagePlant'></td>";
-                echo "<td>" . $record['plantName'] . "</td>";
-                echo "<td>" . $record['plantDesc'] . "</td>";
-                echo "<td>" . "$" . $record['priceDollar'] . "." . $record['priceCent'] . "</td>";
-                echo "<td><a class='btn btn-primary' href='updateProduct.php?idplant=" . $record['idplant'] . "'>Update</a></td>";
+                foreach ($records as $record) {
+                    echo"<div id='$counter'>";
+                    echo "<tr>";
+                    $img = "../" . $record['imgLink'];
+                    if ($record['imgLink'] == null) {
+                        $img = "img/noImg.jpg";
+                    }
 
-                echo '<form action="deleteProduct.php" method="get" onsubmit="return confirmDelete()">';
-                
-                echo "<input type='hidden' name='idplant' value='" . $record['idplant'] . "'>";
-                echo "<td><input type='submit' class='btn btn-danger' name='deleteProduct' value='Remove' id='dlt'></td>";
-                echo "</form>";
-                
-            }
-            echo "</tbody>";
-            echo "</table>";
-            echo "</div>";
-            ?>
+                    echo "<td><a target='_blank' href=" . $img . "><img src=" . $img . " alt='No Image' class='imagePlant'></td>";
+                    echo "<td>" . $record['plantName'] . "</td>";
+                    echo "<td>" . $record['plantDesc'] . "</td>";
+                    echo "<td>" . "$" . $record['priceDollar'] . "." . $record['priceCent'] . "</td>";
+                    echo "<td><a class='btn btn-primary' href='updateProduct.php?idplant=" . $record['idplant'] . "'>Update</a></td>";
 
-            <script>
-                function confirmDelete() {
-                    return confirm("Are you sure you want to delete the product?");
+                    echo '<form action="deleteProduct.php" method="get" onsubmit="return confirmDelete()">';
+
+                    echo "<input type='hidden' name='idplant' value='" . $record['idplant'] . "'>";
+                    echo "<td><input type='submit' class='btn btn-danger' name='deleteProduct' value='Remove' id='dlt'></td>";
+                    echo "</form>";
                 }
-            </script>
+                echo "</tbody>";
+                echo "</table>";
+                echo "</div>";
+                ?>
+
+                <script>
+                    function confirmDelete() {
+                        return confirm("Are you sure you want to delete the product?");
+                    }
+                </script>
 
 
-            <?php
-            if ($_SESSION['prodAdded'] != '') {
-                $_SESSION['prodAdded'] .= ": Has been succesfully added";
-            }
-            ?>
-            <p id="last" style="padding-left: 30px; color:magenta;">
-                <?= $_SESSION['prodAdded']; ?>
-            </p>
+                <?php
+                if ($_SESSION['prodAdded'] != '') {
+                    $_SESSION['prodAdded'] .= ": Has been succesfully added";
+                }
+                ?>
+                <p id="last" style="padding-left: 30px; color:magenta;">
+                    <?= $_SESSION['prodAdded']; ?>
+                </p>
 
-        </div>
-    </div>    
-          
+            </div>
+        </div>    
+
         <script>
             var json;
 
@@ -174,39 +177,32 @@ if (!isset($_SESSION['adminName'])) {
 
             });
 
-            var button;
-           
             function displayResult(json) {
-                 
+
                 $("ul li a").click(function () {
                     var liValue = $(this).attr('id');
                     $.each(json, function (index, value) {
                         if (index === liValue) {
-                          //  alert("the selected value: " + value);
-                            $("#display" + index).html("The '"+index+"' of all prices is "+value);
+                            //  alert("the selected value: " + value);
+                            $("#display" + index).html("The '" + index + "' of all prices is " + value);
                             $("#display" + index).show();
-                            $("#display" + index).css({'color': 'blue',
-                                                        'background-color':'#DCDCDC',
-                                                        'width':'40%',
-                                                        'margin':'auto' });
-                        }else{
-                             $("#display" + index).hide();
+                            $("#display" + index).css({'color': 'white',
+                                'font-size':'20px',
+                                'width': '40%',
+                                'margin': 'auto'});
+                        } else {
+                            $("#display" + index).hide();
                         }
-                       
-                         $("#clear").click(function () {
+
+                        $("#clear").click(function () {
                             $("#display" + index).hide();
                         });
-                        
                     });
                 });
-                 
             }
-            
-             
-         
 
         </script>
-        
+
     </body>
 
     <?php
@@ -224,9 +220,9 @@ if (!isset($_SESSION['adminName'])) {
     }
 
     $_SESSION['prodAdded'] = '';
- 
+
     include '../php/footer.php';  //includes the footer
     ?>
- 
+
 </html>
 
